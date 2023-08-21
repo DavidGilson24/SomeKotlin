5faf39ca982a7c7e346ece6e8eb5cd903188934e
davidgilson24 <cs@david-gilson.com>
2023-08-21 17:51:39 +0200

    Variable file made

---
+++ src/main/kotlin/Variables.kt
@@ -0,0 +1,14 @@
+fun main() {
+
+    // var for variables are used for variables that are subject to change
+    var firstName = "David"
+    var lastName= "Gilson"
+
+    // val is user for variables which are not set to change (e.g. constants)
+    val age = 19
+    val decimal = 3.1415
+
+    println("\nHello world, my first name is $firstName and my last name is $lastName and I am $age years old \n")
+    println("I also have some decimals: $decimal")
+
+}
