9fcafcfd97d9c82fdd183e40a3e0ec75f1fa689f
davidgilson24 <cs@david-gilson.com>
2023-08-24 11:28:38 +0200

    FloatsAndDoubles file done

---
+++ src/main/kotlin/FloatsAndDoubles.kt
@@ -0,0 +1,10 @@
+fun main() {
+
+    val firstNumber = 2.55f // This is a float ( firstNumber: Float = 2.55 does not work to assign a float).
+    val secondNumber = 3.1415 // This is a double
+
+    //They are different in the precision they have with operators and operations
+
+    println("$firstNumber is a float, and $secondNumber is a double!")
+
+}
